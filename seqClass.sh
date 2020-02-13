1374799824b22ed0a31e97f3783e332e4ccad488
ariadnacilleros <ariadna.cilleros@uvic.cat>
2020-02-13 08:39:09 -0800

    exercise1

--- seqClass.sh
+++ seqClass.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-motif=$(echo $2 | tr a-z A-Z)
-if [[ -n $motif ]]; then
-  echo -en "Motif search enabled: looking for motif '$motif' in sequence '$seq'... "
-  if [[ $seq =~ $motif ]]; then
-    echo "todo bien"
+seq=$1
+seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
+if [[ $seq =~ ^[ACGTU]+$ ]]; then
+  if [[ $seq =~ T ]]; then
+    echo "The sequence is DNA"
+  elif [[ $seq =~ U ]]; then
+    echo "The sequence is RNA"
   else
-    echo "nada bien"
+    echo "The sequence can be DNA or RNA"
   fi
+else
+  echo "The sequence is not DNA nor RNA"
 fi
-
